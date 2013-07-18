7747a1ebdf4453516726dd86038b328e5727e599
Santtu Keskinen <laquendi@gmail.com>
2013-07-18 20:23:43 +0300

    Support for per file pivot.

--- BrashMonkeyContentPipelineExtension/SpriterWriter.cs
+++ BrashMonkeyContentPipelineExtension/SpriterWriter.cs
@@ -18,11 +18,15 @@ using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
 using System.Globalization;
 
-namespace BrashMonkeyContentPipelineExtension {
+namespace BrashMonkeyContentPipelineExtension
+{
     [ContentTypeWriter]
-    public class SpriterWriter : ContentTypeWriter<SpriterShadowData> {
-        private bool GetAttributeInt32(XElement p_element, String p_name, out Int32 p_out, Int32 p_default = 0) {
-            if (p_element.Attribute(p_name) != null) {
+    public class SpriterWriter : ContentTypeWriter<SpriterShadowData>
+    {
+        private bool GetAttributeInt32(XElement p_element, String p_name, out Int32 p_out, Int32 p_default = 0)
+        {
+            if (p_element.Attribute(p_name) != null)
+            {
                 return Int32.TryParse(p_element.Attribute(p_name).Value, out p_out);
             }
 
@@ -30,8 +34,10 @@ namespace BrashMonkeyContentPipelineExtension {
             return false;
         }
 
-        private bool GetAttributeFloat(XElement p_element, String p_name, out float p_out, float p_default = 0.0f) {
-            if (p_element.Attribute(p_name) != null) {
+        private bool GetAttributeFloat(XElement p_element, String p_name, out float p_out, float p_default = 0.0f)
+        {
+            if (p_element.Attribute(p_name) != null)
+            {
                 return float.TryParse(p_element.Attribute(p_name).Value, NumberStyles.Any, CultureInfo.InvariantCulture, out p_out);
             }
 
@@ -39,8 +45,10 @@ namespace BrashMonkeyContentPipelineExtension {
             return false;
         }
 
-        private bool GetAttributeBoolean(XElement p_element, String p_name, out bool p_out, bool p_default = true) {
-            if (p_element.Attribute(p_name) != null) {
+        private bool GetAttributeBoolean(XElement p_element, String p_name, out bool p_out, bool p_default = true)
+        {
+            if (p_element.Attribute(p_name) != null)
+            {
                 return Boolean.TryParse(p_element.Attribute(p_name).Value, out p_out);
             }
 
@@ -49,49 +57,74 @@ namespace BrashMonkeyContentPipelineExtension {
         }
 
 
-        protected override void Write(ContentWriter p_output, SpriterShadowData p_value) {
+        protected override void Write(ContentWriter p_output, SpriterShadowData p_value)
+        {
             Int32 l_tmpInt = -1;
             float l_tmpFloat = 0.0f;
             bool l_tmpBool = false;
 
             /// Write the texture dictionary.
-/*            p_output.Write(p_value.XML.Root.Descendants("folder").Count());
-            foreach (XElement l_folder in p_value.XML.Root.Descendants("folder")) {
-                p_output.Write(l_folder.Descendants("file").Count());
-                foreach (XElement l_file in l_folder.Descendants("file")) {
-                    p_output.Write(l_file.Attribute("name").Value);
+            /*            p_output.Write(p_value.XML.Root.Descendants("folder").Count());
+                        foreach (XElement l_folder in p_value.XML.Root.Descendants("folder")) {
+                            p_output.Write(l_folder.Descendants("file").Count());
+                            foreach (XElement l_file in l_folder.Descendants("file")) {
+                                p_output.Write(l_file.Attribute("name").Value);
 
-                    GetAttributeInt32(l_file, "width", out l_tmpInt);
-                    p_output.Write(l_tmpInt);
+                                GetAttributeInt32(l_file, "width", out l_tmpInt);
+                                p_output.Write(l_tmpInt);
 
-                    GetAttributeInt32(l_file, "height", out l_tmpInt);
-                    p_output.Write(l_tmpInt);
-                }
-            }*/
+                                GetAttributeInt32(l_file, "height", out l_tmpInt);
+                                p_output.Write(l_tmpInt);
+                            }
+                        }*/
 
             /// Write the generated Texture content.
             p_output.Write(p_value.Textures.Count());
-            foreach (Texture2DContent l_texture in p_value.Textures) {
+            foreach (Texture2DContent l_texture in p_value.Textures)
+            {
                 p_output.WriteRawObject<Texture2DContent>(l_texture);
             }
 
             /// Write the rectangle location data
             p_output.Write(p_value.Rectangles.Count);
-            foreach (List<Rectangle> l_list in p_value.Rectangles) {
+            foreach (List<Rectangle> l_list in p_value.Rectangles)
+            {
                 p_output.Write(l_list.Count);
-                foreach (Rectangle l_rectangle in l_list) {
+                foreach (Rectangle l_rectangle in l_list)
+                {
                     p_output.WriteRawObject<Rectangle>(l_rectangle);
+                }
+            }
+
+            /// Tmp storage for default file pivots
+            Dictionary<int, Dictionary<int, Vector2>> l_defaultPivot = new Dictionary<int, Dictionary<int, Vector2>>();
+            foreach (XElement l_folder in p_value.XML.Root.Descendants("folder"))
+            {
+                int l_folderId;
+                GetAttributeInt32(l_folder, "id", out l_folderId);
+                foreach (XElement l_file in l_folder.Descendants("file"))
+                {
+                    int l_fileId;
+                    GetAttributeInt32(l_file, "id", out l_fileId);
+
+                    float l_tmpX, l_tmpY;
+                    GetAttributeFloat(l_file, "pivot_x", out l_tmpX);
+                    GetAttributeFloat(l_file, "pivot_y", out l_tmpY, 1.0f);
+
+                    l_defaultPivot.GetOrCreate(l_folderId).Add(l_fileId, new Vector2(l_tmpX, l_tmpY));
                 }
             }
 
             /// Write Entities.
             p_output.Write(p_value.XML.Root.Descendants("entity").Count());
-            foreach (XElement l_entity in p_value.XML.Root.Descendants("entity")) {
+            foreach (XElement l_entity in p_value.XML.Root.Descendants("entity"))
+            {
                 p_output.Write(l_entity.Attribute("name").Value);
 
                 /// Write Animations.
                 p_output.Write(l_entity.Descendants("animation").Count());
-                foreach (XElement l_animation in l_entity.Descendants("animation")) {
+                foreach (XElement l_animation in l_entity.Descendants("animation"))
+                {
                     p_output.Write(l_animation.Attribute("name").Value);
 
                     GetAttributeInt32(l_animation, "length", out l_tmpInt);
@@ -101,17 +134,20 @@ namespace BrashMonkeyContentPipelineExtension {
                     p_output.Write(l_tmpBool);
 
                     /// Write Mainline
-                    foreach (XElement l_mainLine in l_animation.Descendants("mainline")) {
+                    foreach (XElement l_mainLine in l_animation.Descendants("mainline"))
+                    {
 
                         /// Write Key
                         p_output.Write(l_mainLine.Descendants("key").Count());
-                        foreach (XElement l_key in l_mainLine.Descendants("key")) {
+                        foreach (XElement l_key in l_mainLine.Descendants("key"))
+                        {
                             GetAttributeInt32(l_key, "time", out l_tmpInt);
                             p_output.Write(l_tmpInt);
 
                             /// Write Objects
                             p_output.Write(l_key.Descendants("object_ref").Count());
-                            foreach (XElement l_object in l_key.Descendants("object_ref")) {
+                            foreach (XElement l_object in l_key.Descendants("object_ref"))
+                            {
                                 GetAttributeInt32(l_object, "parent", out l_tmpInt, -1);
                                 p_output.Write(l_tmpInt);
 
@@ -126,7 +162,8 @@ namespace BrashMonkeyContentPipelineExtension {
                             }
 
                             p_output.Write(l_key.Descendants("bone_ref").Count());
-                            foreach (XElement l_bone in l_key.Descendants("bone_ref")) {
+                            foreach (XElement l_bone in l_key.Descendants("bone_ref"))
+                            {
                                 GetAttributeInt32(l_bone, "parent", out l_tmpInt, -1);
                                 p_output.Write(l_tmpInt);
 
@@ -143,28 +180,33 @@ namespace BrashMonkeyContentPipelineExtension {
                     //p_output.Write(l_animation.Descendants("timeline").Where(l => l.Attribute("object_type") == null).Count());
                     //foreach (XElement l_timeLine in l_animation.Descendants("timeline").Where(l => l.Attribute("object_type") == null)) {
                     p_output.Write(l_animation.Descendants("timeline").Count());
-                    foreach (XElement l_timeLine in l_animation.Descendants("timeline")) {
+                    foreach (XElement l_timeLine in l_animation.Descendants("timeline"))
+                    {
 
                         p_output.Write(l_entity.Attribute("name").Value);
 
                         /// Write Key                       
                         p_output.Write(l_timeLine.Descendants("key").Count());
-                        foreach (XElement l_key in l_timeLine.Descendants("key")) {
+                        foreach (XElement l_key in l_timeLine.Descendants("key"))
+                        {
                             GetAttributeInt32(l_key, "time", out l_tmpInt);
                             p_output.Write(l_tmpInt);
 
                             GetAttributeInt32(l_key, "spin", out l_tmpInt, 1);
                             p_output.Write(l_tmpInt);
 
-                            if (l_key.Descendants("object").Count() > 0) {
+                            if (l_key.Descendants("object").Count() > 0)
+                            {
                                 p_output.Write(0);///TimelineType.object
 
-                                foreach (XElement l_object in l_key.Descendants("object")) {
-                                    GetAttributeInt32(l_object, "folder", out l_tmpInt);
-                                    p_output.Write(l_tmpInt);
+                                foreach (XElement l_object in l_key.Descendants("object"))
+                                {
+                                    int l_tmpFolder, l_tmpFile;
+                                    GetAttributeInt32(l_object, "folder", out l_tmpFolder);
+                                    p_output.Write(l_tmpFolder);
 
-                                    GetAttributeInt32(l_object, "file", out l_tmpInt);
-                                    p_output.Write(l_tmpInt);
+                                    GetAttributeInt32(l_object, "file", out l_tmpFile);
+                                    p_output.Write(l_tmpFile);
 
                                     GetAttributeFloat(l_object, "x", out l_tmpFloat);
                                     p_output.Write(l_tmpFloat);
@@ -172,10 +214,17 @@ namespace BrashMonkeyContentPipelineExtension {
                                     GetAttributeFloat(l_object, "y", out l_tmpFloat);
                                     p_output.Write(l_tmpFloat);
 
-                                    GetAttributeFloat(l_object, "pivot_x", out l_tmpFloat);
+
+                                    if (!GetAttributeFloat(l_object, "pivot_x", out l_tmpFloat))
+                                    {
+                                        l_tmpFloat = l_defaultPivot[l_tmpFolder][l_tmpFile].X;
+                                    }
                                     p_output.Write(l_tmpFloat);
 
-                                    GetAttributeFloat(l_object, "pivot_y", out l_tmpFloat, 1.0f);
+                                    if (!GetAttributeFloat(l_object, "pivot_y", out l_tmpFloat))
+                                    {
+                                        l_tmpFloat = l_defaultPivot[l_tmpFolder][l_tmpFile].Y;
+                                    }
                                     p_output.Write(l_tmpFloat);
 
                                     GetAttributeFloat(l_object, "angle", out l_tmpFloat);
@@ -190,10 +239,13 @@ namespace BrashMonkeyContentPipelineExtension {
                                     GetAttributeFloat(l_object, "a", out l_tmpFloat, 1.0f);
                                     p_output.Write(l_tmpFloat);
                                 }
-                            } else {
+                            }
+                            else
+                            {
                                 p_output.Write(1);///TimelineType.bone
 
-                                foreach (XElement l_bone in l_key.Descendants("bone")) {
+                                foreach (XElement l_bone in l_key.Descendants("bone"))
+                                {
                                     GetAttributeFloat(l_bone, "x", out l_tmpFloat);
                                     p_output.Write(l_tmpFloat);
 
@@ -208,7 +260,7 @@ namespace BrashMonkeyContentPipelineExtension {
 
                                     GetAttributeFloat(l_bone, "angle", out l_tmpFloat);
                                     p_output.Write(l_tmpFloat);
-                                    
+
                                     GetAttributeFloat(l_bone, "scale_x", out l_tmpFloat, 1.0f);
                                     p_output.Write(l_tmpFloat);
 
@@ -222,11 +274,13 @@ namespace BrashMonkeyContentPipelineExtension {
             }
         }
 
-        public override string GetRuntimeType(TargetPlatform p_targetPlatform) {
+        public override string GetRuntimeType(TargetPlatform p_targetPlatform)
+        {
             return typeof(BrashMonkeySpriter.CharacterModel).AssemblyQualifiedName;
         }
 
-        public override string GetRuntimeReader(TargetPlatform p_targetPlatform) {
+        public override string GetRuntimeReader(TargetPlatform p_targetPlatform)
+        {
             return typeof(BrashMonkeySpriter.Content.SpriterReader).AssemblyQualifiedName;
         }
     }
