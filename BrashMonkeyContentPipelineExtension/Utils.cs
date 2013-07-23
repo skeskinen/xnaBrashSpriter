40995ebb0043180a155679e861291e3890683c6e
Geoff Lodder <geoff.lodder@gmail.com>
2013-07-23 12:59:54 -0400

    Merged Laquendi's fprk into the project. Thanks.

--- BrashMonkeyContentPipelineExtension/Utils.cs
+++ BrashMonkeyContentPipelineExtension/Utils.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BrashMonkeyContentPipelineExtension
-{
-    static class Utils
-    {
+namespace BrashMonkeyContentPipelineExtension {
+    static class Utils {
         /// Dictionary extension method
         /// Taken from: http://stackoverflow.com/questions/16192906/net-dictionary-get-or-create-new
         public static TValue GetOrCreate<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
-            where TValue : new()
-        {
+            where TValue : new() {
             TValue val;
 
-            if (!dict.TryGetValue(key, out val))
-            {
+            if (!dict.TryGetValue(key, out val)) {
                 val = new TValue();
                 dict.Add(key, val);
             }
--- BrashMonkeySpriterGameTest/BrashMonkeySpriterGameTest/Program.cs
+++ BrashMonkeySpriterGameTest/BrashMonkeySpriterGameTest/Program.cs
@@ -11,15 +11,12 @@ using System;
 
 namespace BrashMonkeySpriterGameTest {
 #if WINDOWS || XBOX
-    static class Program
-    {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main(string[] args)
-        {
-            using (Game1 game = new Game1())
-            {
+        static void Main(string[] args) {
+            using (Game1 game = new Game1()) {
                 game.Run();
             }
         }
